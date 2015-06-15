efad1bb61fb6cff94a348e3de4a7c2bfa19033c7
hardok <hardok@tlu.ee>
2015-06-15 10:05:37 +0300

    Kustutasin palju demo.js

--- demo/demo.js
+++ demo/demo.js
@@ -49,153 +49,7 @@ app.config(function ($routeProvider) {
 
 });
 
-//
-// `$drag` example: drag to dismiss
-//
-app.directive('dragToDismiss', function ($drag, $parse, $timeout) {
-    return {
-        restrict: 'A',
-        compile: function (elem, attrs) {
-            var dismissFn = $parse(attrs.dragToDismiss);
-            return function (scope, elem, attrs) {
-                var dismiss = false;
 
-                $drag.bind(elem, {
-                    constraint: {
-                        minX: 0,
-                        minY: 0,
-                        maxY: 0
-                    },
-                    move: function (c) {
-                        if (c.left >= c.width / 4) {
-                            dismiss = true;
-                            elem.addClass('dismiss');
-                        } else {
-                            dismiss = false;
-                            elem.removeClass('dismiss');
-                        }
-                    },
-                    cancel: function () {
-                        elem.removeClass('dismiss');
-                    },
-                    end: function (c, undo, reset) {
-                        if (dismiss) {
-                            elem.addClass('dismitted');
-                            $timeout(function () {
-                                scope.$apply(function () {
-                                    dismissFn(scope);
-                                });
-                            }, 400);
-                        } else {
-                            reset();
-                        }
-                    }
-                });
-            };
-        }
-    };
-});
-
-//
-// Another `$drag` usage example: this is how you could create 
-// a touch enabled "deck of cards" carousel. See `carousel.html` for markup.
-//
-app.directive('carousel', function () {
-    return {
-        restrict: 'C',
-        scope: {},
-        controller: function ($scope) {
-            this.itemCount = 0;
-            this.activeItem = null;
-
-            this.addItem = function () {
-                var newId = this.itemCount++;
-                this.activeItem = this.itemCount == 1 ? newId : this.activeItem;
-                return newId;
-            };
-
-            this.next = function () {
-                this.activeItem = this.activeItem || 0;
-                this.activeItem = this.activeItem == this.itemCount - 1 ? 0 : this.activeItem + 1;
-            };
-
-            this.prev = function () {
-                this.activeItem = this.activeItem || 0;
-                this.activeItem = this.activeItem === 0 ? this.itemCount - 1 : this.activeItem - 1;
-            };
-        }
-    };
-});
-
-app.directive('carouselItem', function ($drag) {
-    return {
-        restrict: 'C',
-        require: '^carousel',
-        scope: {},
-        transclude: true,
-        template: '<div class="item"><div ng-transclude></div></div>',
-        link: function (scope, elem, attrs, carousel) {
-            scope.carousel = carousel;
-            var id = carousel.addItem();
-
-            var zIndex = function () {
-                var res = 0;
-                if (id == carousel.activeItem) {
-                    res = 2000;
-                } else if (carousel.activeItem < id) {
-                    res = 2000 - (id - carousel.activeItem);
-                } else {
-                    res = 2000 - (carousel.itemCount - 1 - carousel.activeItem + id);
-                }
-                return res;
-            };
-
-            scope.$watch(function () {
-                return carousel.activeItem;
-            }, function (n, o) {
-                elem[0].style['z-index'] = zIndex();
-            });
-
-
-            $drag.bind(elem, {
-                constraint: { minY: 0, maxY: 0 },
-                adaptTransform: function (t, dx, dy, x, y, x0, y0) {
-                    var maxAngle = 15;
-                    var velocity = 0.02;
-                    var r = t.getRotation();
-                    var newRot = r + Math.round(dx * velocity);
-                    newRot = Math.min(newRot, maxAngle);
-                    newRot = Math.max(newRot, -maxAngle);
-                    t.rotate(-r);
-                    t.rotate(newRot);
-                },
-                move: function (c) {
-                    if (c.left >= c.width / 4 || c.left <= -(c.width / 4)) {
-                        elem.addClass('dismiss');
-                    } else {
-                        elem.removeClass('dismiss');
-                    }
-                },
-                cancel: function () {
-                    elem.removeClass('dismiss');
-                },
-                end: function (c, undo, reset) {
-                    elem.removeClass('dismiss');
-                    if (c.left >= c.width / 4) {
-                        scope.$apply(function () {
-                            carousel.next();
-                        });
-                    } else if (c.left <= -(c.width / 4)) {
-                        scope.$apply(function () {
-                            carousel.next();
-                        });
-                    }
-                    reset();
-                }
-            });
-        }
-    };
-});
 
 app.controller('plantslist', function ($scope, plants) {
     $scope.plants = plants;
@@ -250,9 +104,7 @@ app.controller('MainController', function ($rootScope, $scope, $route, Azureserv
         $rootScope.loading = false;
     });
 
-    // Fake text i used here and there.
-    $scope.lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel explicabo, aliquid eaque soluta nihil eligendi adipisci error, illum corrupti nam fuga omnis quod quaerat mollitia expedita impedit dolores ipsam. Obcaecati.';
-
+ 
     // 
     // 'Scroll' screen
     // 
@@ -281,41 +133,12 @@ app.controller('MainController', function ($rootScope, $scope, $route, Azureserv
         alert('You submitted the login form');
     };
 
-    // 
-    // 'Drag' screen
-    // 
-    $scope.notices = [];
 
-    for (var j = 0; j < 10; j++) {
-        $scope.notices.push({ icon: 'envelope', message: 'Notice ' + (j + 1) });
-    }
-
-    $scope.deleteNotice = function (notice) {
-        var index = $scope.notices.indexOf(notice);
-        if (index > -1) {
-            $scope.notices.splice(index, 1);
-        }
-    };
 });
 
 
 
-// Number picker stuff
-/*
-var demo = angular.module('demo', ['angularNumberPicker']);
 
-demo.controller('DemoController', ['$scope', function($scope) {
-		console.log($scope.input);
-        $scope.input = {
-            num: 0
-        };
-
-        $scope.getNumber = function() {
-            alert('The number is: [' + $scope.input.num + ']');
-        };
-
-}]);
-*/
 
 //	Graafik
 
